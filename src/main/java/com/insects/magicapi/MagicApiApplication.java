fc9b35db3bdd05fa598a1ac4860c5bdd73ef2303
lihao25 <15937811122@163.com>
2022-05-11 00:54:58 +0800

    magic-api#first try

---
+++ src/main/java/com/insects/magicapi/MagicApiApplication.java
@@ -0,0 +1,14 @@
+package com.insects.magicapi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicApiApplication.class, args);
+    }
+
+}
